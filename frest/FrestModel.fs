d0f36160e5a605c5edc69312b2aab24f317c4fb9
Bryan <shadowwolf007@gmail.com>
2016-03-04 09:57:19 -0600

    Adding 2 more files

---
+++ frest/FrestModel.fs
@@ -0,0 +1,27 @@
+﻿module FrestModel
+
+type frest_header = { 
+    name: string; 
+    value: string;
+}
+
+type frest_request = 
+    Get
+    | Post
+    | Put 
+    | Patch 
+
+
+type url = string
+
+type frest_content = {
+    name: string;
+    value: string;
+}
+
+type frest_model = {
+    headers: List<frest_header>;
+    request: frest_request;
+    url: url;
+    content: List<frest_content>;
+}
---
+++ frest/PatternMatchers.fs
@@ -0,0 +1,20 @@
+﻿module PatternMatchers
+
+let (|InvariantEqual|_|) (str : string) arg =
+    if String.Compare(str, arg, StringComparison.OrdinalIgnoreCase) = 0 then 
+        Some() 
+    else 
+        None
+
+let (|StartsWith|_|) (str : string) (arg : string) =
+    if arg.StartsWith(str, StringComparison.OrdinalIgnoreCase) then 
+        Some()
+    else
+        None
+
+let (|HasMatch|_|) (pattern : string) (arg : string) =
+    let r = Regex(pattern).Match(arg)
+    if r.Success then
+        Some()
+    else
+        None
