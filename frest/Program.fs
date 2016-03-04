16b930235178dbc276db834eb95015be5ec14640
Bryan <shadowwolf007@gmail.com>
2016-03-04 09:57:04 -0600

    WIP

--- frest/Program.fs
+++ frest/Program.fs
@@ -1,7 +1,42 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿module Program
+open FrestModel
+open PatternMatchers
+open System
+open System.Text.RegularExpressions
+
+// Parameters:
+// frest [header:value]{0+} REQUEST URL [KEY=Value]{0+}
+// 
+
+
+
+let defaultOpt arg model =
+    printfn "Unknown option %A" arg
+    model
+
+let processArgs cmdLine model =
+    let rec processor list innerModel =  
+        let doArgs args m : frest_model = 
+            match args with 
+                | InvariantEqual "Post" -> {m with request = Post}
+                | InvariantEqual "Get" -> {m with request = Get}
+                | InvariantEqual "Put" -> {m with request = Put}
+                | InvariantEqual "Patch" -> {m with request = Patch}
+                | StartsWith "http" -> {m with url = args}
+                | StartsWith "www" -> {m with url = args}
+                | HasMatch "([A-Z]+)=([A-Z]+)" -> {m with content = m.content :: args}
+                | _ -> defaultOpt args m        
+           
+        match list with 
+            | [] -> innerModel       
+            | head :: tail -> processor tail (doArgs head innerModel)
+    processor cmdLine model
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    //printfn "%A" argv
+    let args = argv |> List.ofSeq
+    let model = { frest_model.content = []; frest_model.headers = []; frest_model.request = Get; frest_model.url = "" }
+    let processed = processArgs args model
+    printfn "%A" processed
     0 // return an integer exit code
