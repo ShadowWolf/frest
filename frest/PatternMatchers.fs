902b8b8b8f500e508e164bb43d7f860dda3f1ddb
Bryan <shadowwolf007@gmail.com>
2016-03-05 08:27:18 -0600

    Finish regex options so that things match up

--- frest/PatternMatchers.fs
+++ frest/PatternMatchers.fs
@@ -14,8 +14,13 @@ let (|StartsWith|_|) (str : string) (arg : string) =
     else
         None
 
+// Pointless literal, I'm just playing with it :-D
+[<Literal>]
+let regexOptions = RegexOptions.IgnoreCase ||| RegexOptions.IgnorePatternWhitespace
+let regexTimeout = TimeSpan.FromSeconds(5.0)
+
 let (|HasMatch|_|) (pattern : string) (arg : string) =
-    let r = Regex(pattern).Match(arg)
+    let r = Regex(pattern, regexOptions, regexTimeout).Match(arg)
     if r.Success then
         Some()
     else
