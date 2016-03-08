df2a9b76aa23b6e568b63677a4ccb261e9610643
Bryan <shadowwolf007@gmail.com>
2016-03-08 11:40:02 -0600

    Playing with some formatting. C# methods make it weird.

--- frest/RequestModel.fs
+++ frest/RequestModel.fs
@@ -9,11 +9,17 @@ let makeDictionary content =
     |> List.map (fun m -> (m.name, m.value)) 
     |> dict
 
-let mapJson content =
-    JsonConvert.SerializeObject(content)
+let makeJsonContent (model : frest_model) =
+    if (String.IsNullOrEmpty model.jsonFile = true) then
+        makeDictionary model.content
+        |> JsonConvert.SerializeObject
+    else
+        System.IO.File.ReadAllText model.jsonFile
+
 
 let mapHeaders (headers : Set<frest_header>) = 
-    headers |> Set.map (fun m -> (m.name, m.value))
+    headers 
+    |> Set.map (fun m -> (m.name, m.value))
 
 let mapQuery request content = 
     match request with
@@ -25,21 +31,16 @@ let mapBody request content =
         | Post | Patch | Put -> HttpRequestBody.TextRequest content
         | _ -> HttpRequestBody.TextRequest("")
 
-let mapJsonContent (model : frest_model) =
-    if (String.IsNullOrEmpty model.jsonFile = true) then
-        makeDictionary(model.content) |> mapJson
-    else
-        System.IO.File.ReadAllText(model.jsonFile)
+
 
 let send (model : frest_model) =
-    let requestType = model.request.ToString()
     printfn "Performing request %A to url %A" model.request model.url
     try
         Http.RequestString(model.url, 
-                       httpMethod=model.request.ToString(), 
-                       headers=(mapHeaders model.headers), 
-                       query=(mapQuery model.request model.content), 
-                       body=(mapBody model.request (mapJsonContent model)))
+                       httpMethod = model.request.ToString(), 
+                       headers = (mapHeaders model.headers), 
+                       query = (mapQuery model.request model.content), 
+                       body = (mapBody model.request (makeJsonContent model)))
     with
         | :? System.Net.WebException as webEx -> webEx.ToString()
         
@@ -48,11 +49,11 @@ let addContentType (model : frest_model) =
         model
     else
         match model.request with
-            | Post | Put | Patch -> {model with headers = model.headers.Add(build_header "content-type:application/json" ":")}
+            | Post | Put | Patch -> {model with headers = model.headers.Add (build_header "content-type:application/json" ":")}
             | _ -> model      
 
 let build_request (model : frest_model) = 
-    if (String.IsNullOrEmpty(model.jsonFile) = false && model.content.IsEmpty = false) then
+    if ( (String.IsNullOrEmpty model.jsonFile) = false && model.content.IsEmpty = false) then
         "Both a json file and a list of key/value pairs was supplied."
     else 
         let updatedModel = addContentType model
